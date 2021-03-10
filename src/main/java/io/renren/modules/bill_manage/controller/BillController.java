b59b86808018ceba8362ef64d3e0fb424f2eebb4
Brilliant ZC <2411464044@qq.com>
2021-03-10 09:38:31 +0800

    一些更改

--- src/main/java/io/renren/modules/bill_manage/controller/BillController.java
+++ src/main/java/io/renren/modules/bill_manage/controller/BillController.java
@@ -3,6 +3,9 @@ package io.renren.modules.bill_manage.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.renren.modules.bill_manage.entity.DayBillEntity;
+import io.renren.modules.bill_manage.service.DayBillService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +33,8 @@ import io.renren.common.utils.R;
 public class BillController {
     @Autowired
     private BillService billService;
+    @Autowired
+    private DayBillService dayBillService;
 
     /**
      * 列表
@@ -62,6 +67,53 @@ public class BillController {
     public R save(@RequestBody BillEntity bill){
 		billService.save(bill);
 
+        //在添加一条账目时将其bill_id 判断在日账目中是否存在，如果不存在就添加新的一个日账单中,存在就不做处理
+        QueryWrapper<DayBillEntity> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("day_billid",bill.getBillId());
+        DayBillEntity dayBillEntity=dayBillService.getOne(queryWrapper);
+        //如果存在
+        if(dayBillEntity!=null)
+        {
+            //拿到日账单实体对其收支进行更新
+            if(bill.getBillInout().equals("1")){
+                dayBillEntity.setDayIncome(dayBillEntity.getDayIncome()+bill.getBillAccount());
+            }
+            else dayBillEntity.setDayOutcome(dayBillEntity.getDayOutcome()+bill.getBillAccount());
+            dayBillService.updateById(dayBillEntity);
+        }
+        //如果不存在
+        else {
+            DayBillEntity dayBillEntity1 = new DayBillEntity();
+            dayBillEntity1.setDayBillid(bill.getBillId());
+            System.out.println();
+            if(bill.getBillInout().equals("1")){
+                dayBillEntity1.setDayIncome((float) 0+bill.getBillAccount());
+                dayBillEntity1.setDayOutcome((float) 0);
+            }
+            else {
+                dayBillEntity1.setDayIncome((float) 0);
+                dayBillEntity1.setDayOutcome((float) 0 + bill.getBillAccount());
+            }
+            dayBillEntity1.setDayYear(bill.getBillId() / 10000);
+            dayBillEntity1.setDayMon(bill.getBillId() % 10000 / 100);
+            dayBillEntity1.setDayDay(bill.getBillId() / 1000000);
+            //求星期数
+            String[] weeks = {"星期日","星期一","星期二","星期三","星期四","星期五","星期六"};
+            int y=dayBillEntity1.getDayYear()%100;
+            int c=dayBillEntity1.getDayYear()/100;
+            int m=dayBillEntity1.getDayMon();
+            int d=dayBillEntity1.getDayDay();
+            if(m==1||m==2){
+                y--;
+                m+=12;
+            }
+            int w = y + y / 4 + c / 4 - 2 * c + 13 * (m + 1) / 5 + d - 1;//蔡勒公式的公式
+            while (w < 0) w += 7;//确保余数为正
+            w %= 7;
+            System.out.println(w);
+            dayBillEntity1.setDayWeek(weeks[w]);
+            dayBillService.save(dayBillEntity1);
+        }
         return R.ok();
     }
 
